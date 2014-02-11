36792f95bc78db911c96eebc0951f03265f8b9ef
U-DMN1\a249261 <a249261@LA979097.fmr.com>
2014-02-10 21:06:08 -0500

    another

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bashrc vimrc vimrc_my_settings zshrc oh-my-zsh private scrotwm.conf Xresources"    # list of files/folders to symlink in homedir
+files="bashrc bash_common bash_profile vimrc vimrc_my_settings zshrc oh-my-zsh private scrotwm.conf Xresources"    # list of files/folders to symlink in homedir
 
 ##########
 
