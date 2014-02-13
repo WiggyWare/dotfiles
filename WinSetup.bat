1ec66a819fef4ed594c40ddeafbfe2e2f27badd1
U-DMN1\a249261 <a249261@LA979097.fmr.com>
2014-02-13 13:57:25 -0500

    bunch of updates

--- WinSetup.bat
+++ WinSetup.bat
@@ -3,5 +3,5 @@ REM You must run this as Administrator.
 REM cd %USERPROFILE%
 del %USERPROFILE%\.vimrc
 del %USERPROFILE%\.vimrc_my_settings
-mklink %USERPROFILE%\.vimrc C:\cygwin\home\%USERNAME%\dotfiles\vimrc
-mklink %USERPROFILE%\.vimrc_my_settings C:\cygwin\home\%USERNAME%\dotfiles\vimrc_my_settings
+mklink /H %USERPROFILE%\.vimrc C:\cygwin\home\%USERNAME%\dotfiles\vimrc
+mklink /H %USERPROFILE%\.vimrc_my_settings C:\cygwin\home\%USERNAME%\dotfiles\vimrc_my_settings
