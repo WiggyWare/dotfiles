1b4ff0514cc4b5a5a6edfd98f275cd85d93d746e
U-DMN1\a249261 <a249261@LA979097.fmr.com>
2014-02-13 14:13:14 -0500

    win

--- WinSetup.bat
+++ WinSetup.bat
@@ -3,5 +3,7 @@ REM You must run this as Administrator.
 REM cd %USERPROFILE%
 del %USERPROFILE%\.vimrc
 del %USERPROFILE%\.vimrc_my_settings
+rmdir %USERPROFILE%\.vim
 mklink /H %USERPROFILE%\.vimrc C:\cygwin\home\%USERNAME%\dotfiles\vimrc
 mklink /H %USERPROFILE%\.vimrc_my_settings C:\cygwin\home\%USERNAME%\dotfiles\vimrc_my_settings
+mklink /J %USERPROFILE%\.vim C:\cygwin\home\%USERNAME%\.vim
